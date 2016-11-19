f5d40a2cb9fddd143d4734a03c333cb19164f51b
Naitop <topdoraemon@gmail.com>
2016-11-19 15:00:42 +0700

    TakePhoto

--- app/src/main/java/com/aoyama/atc/myfriend/SignUpActivity.java
+++ app/src/main/java/com/aoyama/atc/myfriend/SignUpActivity.java
@@ -1,7 +1,13 @@
 package com.aoyama.atc.myfriend;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +20,7 @@ public class SignUpActivity extends AppCompatActivity {
     private ImageView avataImageView,takePhotoImageView;
     private Button button;
     private String nameString,userString,passwordString;
+    private Uri uri;
 
 
     @Override
@@ -45,8 +52,6 @@ public class SignUpActivity extends AppCompatActivity {
                     MyAlert myAlert = new MyAlert(SignUpActivity.this,R.drawable.doremon48,
                             "ข้อมูลไม่สมบูรณ์","กรุณากรอกข้อมูลสำคัญ !!!");
                     myAlert.myDialog();
-
-
                 } //if
 
             }// Onclick
@@ -57,11 +62,47 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
             } //onclick takePhotoImage
         });
 
+
+        //avata Controller
+        avataImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+
+
     } //Main Method
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //ถ้า ส่งค่ามา สถานะเป็น 0 และมีค่ามา
+        if ((requestCode==0)&&(resultCode==RESULT_OK)) {
+
+            //Take Photo OK
+            Log.d("19NovV1", "Take Photo OK");
+             uri = data.getData();
+
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                avataImageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                Log.d("19NovV1", "e==>" + e.toString());
+            }
+            //Setup Image Take to ImageView
+
+        } //if
+
+
+    } //onActivityResult
 } // Main Class
